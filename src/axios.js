15098911b4d6a43bb41609f1879002b8b12b1d38
Collins Appiah <appiahc86@gmail.com>
2023-10-13 06:07:33 +0100

    updated

--- src/axios.js
+++ src/axios.js
@@ -4,8 +4,8 @@ import {useHomeStore} from "./store/home.js";
 import {useComponentStore} from "@/store/componentStore";
 
 
-axios.defaults.baseURL = "http://localhost:3000";
-// axios.defaults.baseURL = "https://res.nantylotto.com";
+// axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://res.nantylotto.com";
 // axios.defaults.baseURL = "http://192.168.43.122:3000";
 axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
 axios.defaults.headers.patch['Content-Type'] = 'application/json; charset=utf-8';
